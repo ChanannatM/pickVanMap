766a4964db63c69e9b57db1739d9245aca871574
ChanannatM <chanannat10969@gmail.com>
2021-10-29 16:55:27 +0700

    MapViewDirection

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 const App = () => {
+  const [state, setState] = useState({
+    pickupCords:{
+      latitude: 13.64788,
+      longitude: 100.679709,
+      latitudeDelta: 0.03,
+      longitudeDelta: 0.005,
+    },
+    droplocationCords:{
+      latitude: 13.274305,
+      longitude: 100.928648,
+      latitudeDelta: 0.03,
+      longitudeDelta: 0.005,
+    }
+  })
+
+  const {pickupCords,droplocationCords} = state
+  const GOOGLE_MAPS_APIKEY = 'AIzaSyB1-M05tikKf77sE5ccykQCdto5cuSPP0I';
+
   return (
     <View style={styles.container}>
       <MapView
         style={StyleSheet.absoluteFill}
-        initialRegion={{
-          latitude: 8.641631,
-          longitude: 99.897324,
-          latitudeDelta: 0.03,
-          longitudeDelta: 0.005,
-        }}
-      />
+        initialRegion={pickupCords}
+        > 
+        <MapViewDirections
+          origin={pickupCords}
+          destination={droplocationCords}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+        />
+      </MapView>
+     
     </View>
   );
 };
